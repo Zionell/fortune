83db1a0c40f8694081b50713f75d5c4307c40855
Zionell <texas2017polet>
2022-12-18 12:36:34 +0600

    fix cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/api', router);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/dist', '/index.html'), function (err) {
+    res.sendFile(path.join('./client/dist', '/index.html'), function (err) {
         if (err) {
             res.status(500).send(err)
         }
