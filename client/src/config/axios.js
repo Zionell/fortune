cda51b1dd4435e7d78f537cacb04064131426f14
Zionell <texas2017polet>
2022-12-18 08:12:53 +0600

    fix cors

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const base_url =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://fortune-for-sales.herokuapp.com";
+    : "https://fortune-sooty.vercel.app/";
 
 export const axiosDefault = axios.create({
   baseURL: base_url,
