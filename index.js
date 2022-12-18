4363fbddb5bfa97dfbddd2372f11dc3670c8c94b
Zionell <texas2017polet>
2022-12-18 12:53:13 +0600

    fix cors

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use(express.json());
 
 app.use('/api', router);
 
-app.get('/(.*).css', (req, res) => {
-    res.sendFile(path.join(__dirname, '/css/(.*).css'), function (err) {
-        if (err) {
-            res.status(500).send(err)
-        }
-    })
-})
+// app.get('/(.*).css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/css/(.*).css'), function (err) {
+//         if (err) {
+//             res.status(500).send(err)
+//         }
+//     })
+// })
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'), function (err) {
